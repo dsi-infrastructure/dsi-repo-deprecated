67a9355b393cd05a2cc55dac96a7f9f17b0dcc10
Philippe EYCHART <philippe.eychart@informatique.gov.pf>
2014-11-06 09:59:16 -1000

    suppression de chef-awstat

--- cookbooks/chef-awstats/metadata.rb
+++
@@ -1,9 +0,0 @@
-name             'chef-awstats'
-maintainer       'PE, pf.'
-maintainer_email 'peychart@mail.pf'
-license          'GNU Public License 3.0'
-description      'Installs/Configures chef-awstats'
-long_description 'Installs/Configures chef-awstats'
-version          '0.1.0'
-depends          'awstats'
-
