1b5b4ba16913c2e27a958c8370f5e28a279419e0
Philippe EYCHART <philippe.eychart@informatique.gov.pf>
2014-11-14 10:18:56 -1000

    changement de quelques valeurs par defaut sur chef-caimito

--- cookbooks/chef-caimito/attributes/default.rb
+++ cookbooks/chef-caimito/attributes/default.rb
@@ -17,13 +17,13 @@ default['chef-caimito']['resource']['file']['resource.dir'] = default['chef-caim
 default['chef-caimito']['resource']['openstack']['cloud.store'] = 'caimito'
 default['chef-caimito']['resource']['openstack']['cloud.username'] = 'system:root'
 default['chef-caimito']['resource']['openstack']['cloud.api.key_password'] = 'testpass'
-default['chef-caimito']['resource']['openstack']['cloud.url'] = 'http://swiftauth:8080/auth/v1.0'
+default['chef-caimito']['resource']['openstack']['cloud.url'] = 'http://swift:8080/auth/v1.0'
 #default['chef-caimito']['resource']['Amazon S3']['cloud.username'] = 'SLOS11111-1:SL11111'
 #default['chef-caimito']['resource']['Amazon S3']['cloud.api.key_password'] = 'fffffffffffffffffffffffffffffffffff'
 #default['chef-caimito']['resource']['Amazon S3']['url'] = 'https://dal05.objectstorage.softlayer.net/auth/v1.0/'
 #default['chef-caimito']['resource']['Amazon S3']['store'] = 'caimito'
 
 default['chef-caimito']['ldap']['enable'] = false
-default['chef-caimito']['ldap']['ldap.url'] = 'ldap://ldap_server:389'
+default['chef-caimito']['ldap']['ldap.url'] = 'ldap://ldap:389'
 default['chef-caimito']['ldap']['ldap.authentication'] = 'simple'
 default['chef-caimito']['ldap']['ldap.domain'] = 'domain'
