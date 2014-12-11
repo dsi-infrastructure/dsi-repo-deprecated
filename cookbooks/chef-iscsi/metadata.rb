1a0e48a6492dbe903c202414ace4117e8f18579a
Philippe EYCHART <philippe.eychart@informatique.gov.pf>
2014-12-10 17:14:28 -1000

    pb de la config delegate

--- cookbooks/chef-iscsi/metadata.rb
+++
@@ -1,8 +0,0 @@
-name             'chef-iscsi'
-maintainer       'PE, pf.'
-maintainer_email 'peychart@mail.pf'
-license          'GNU Public License 3.0'
-description      'Installs/Configures chef-iscsi'
-long_description 'Installs/Configures chef-iscsi'
-version          '0.1.0'
-
